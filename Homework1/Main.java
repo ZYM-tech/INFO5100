4ab3cf80b24c229bea4634a7c23ba9517eae6cd0
ZYM <yiming.zhang1024@gmail.com>
2021-09-24 15:11:27 -0700

    Question 4

--- Homework1/Main.java
+++ Homework1/Main.java
@@ -13,5 +13,10 @@ public class Main {
         //Question 3 Testcase
         int[] nums3 = {2,3,4,2,2,3,5,7};
         System.out.println("Question 3: "+ Question3.firstNonRepeated(nums3));
+
+        //Question 4 Testcase
+        String s = "ab";
+        String t = "abc";
+        System.out.println(Question4.isContain(s,t));
     }
 }
--- Homework1/Question4.java
+++ Homework1/Question4.java
@@ -1,2 +1,17 @@
 public class Question4 {
+    public static boolean isContain(String s, String t){
+        int[] bucket = new int[26];
+        for(int i=0; i<t.length();i++){
+            bucket[t.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            bucket[s.charAt(i)-'a']--;
+        }
+        for(int i=0;i<bucket.length;i++){
+            if(bucket[i]<0){
+                return false;
+            }
+        }
+        return true;
+    }
 }

ab24e3abea82fa2e997f6eae207001f97d8cf879
ZYM <yiming.zhang1024@gmail.com>
2021-09-24 02:14:25 -0700

    Question 1

--- Homework1/Question1.java
+++ Homework1/Question1.java
@@ -1,2 +1,16 @@
 public class Question1 {
+    public static int sum(int[] nums){
+        int sum = 0;
+        for(int i=0; i<nums.length; i++){
+            int count = 0;
+            for(int k=0; k<nums.length;k++){
+                if(nums[k] == nums[i]){ //count how many same number
+                    count++;
+                }
+            }
+            if(count == 1){
+                sum = sum + nums[i];
+            }
+        }return sum;
+    }
 }

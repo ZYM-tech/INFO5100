bb271f2856a3a2f4c783d306f716e6f1f671a224
ZYM <yiming.zhang1024@gmail.com>
2021-10-07 22:43:52 -0700

    LeetCode 3 - HW2 Optimal

--- Homework2/HW2_LC3.java
+++ Homework2/HW2_LC3.java
@@ -1,26 +1,14 @@
-import java.util.ArrayList;
 public class HW2_LC3 {
+    //Time: O(n) Space:O(1)
     public static void moveZeroes(int[] nums) {
-        ArrayList<Integer> result = new ArrayList<>();
-        for(int i=0; i<nums.length;i++){
-            result.add(nums[i]);
-        }
-        int count = 0;
-        for(int i=0; i<result.size();i++){
-            if(result.get(i) == 0){
-                result.remove(i);
-                i--;
-                count++;
+        int j = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
             }
-        }
-        while(count >0 ){
-            result.add(0);
-            count--;
-        }
-        int i=0;
-        for(int k : result){
-            nums[i] = k;
-            i++;
         }
     }
 }

f152411b767e1644ee02db66a020cafd24febafc
ZYM <yiming.zhang1024@gmail.com>
2021-12-09 22:34:11 -0800

    Homework 6 Q2

--- Homework6/Q2/DrivingLicenseApplication.java
+++ Homework6/Q2/DrivingLicenseApplication.java
@@ -9,27 +9,26 @@ public class DrivingLicenseApplication {
     public static void main(String[] args) throws Exception {
 
         Scanner text = new Scanner(System.in);
-
-        System.out.println("Please Enter Birth Year (Format: YYYY)");
-        String birthYear = text.nextLine();
-        int year = Integer.parseInt(birthYear);
-
         System.out.println("Please Enter Birth Month (Format: MM)");
         String birMonth = text.nextLine();
         int month = Integer.parseInt(birMonth);
 
         System.out.println("Please Enter Birth Date (Format: DD)");
-        String birDate = text.nextLine();  // Read user input
+        String birDate = text.nextLine();
         int day = Integer.parseInt(birDate);
 
+        System.out.println("Please Enter Birth Year (Format: YYYY)");
+        String birthYear = text.nextLine();
+        int year = Integer.parseInt(birthYear);
+
         Date now = new Date();
-        Date birth = new GregorianCalendar(year, month, day).getTime();
+        Date birth = new GregorianCalendar(month, day, year).getTime();
 
         try{
-            int difference = (int) (ChronoUnit.DAYS.between( birth.toInstant(), now.toInstant()));
-            int age = difference/365;
+            int count = (int) (ChronoUnit.DAYS.between( birth.toInstant(), now.toInstant()));
+            int age = count/365;
             if (age < 16) {
-                throw new Exception("Sorry, Your age is under 14 which is not eligible for applying a driving license");
+                throw new Exception("Sorry, Your age is under 16 which is not eligible for applying a driving license");
             }
         }catch (Exception e){
             e.printStackTrace();
